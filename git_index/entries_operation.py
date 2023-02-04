1e42c6957fa743db4494dd9a161989300b838c47
Sayama0310 <montblanc3060@gmail.com>
2023-02-04 11:47:17 +0900

    [feat] get_entries_length()の使用する関数のインターフェースを決定

--- git_index/entries_operation.py
+++ git_index/entries_operation.py
@@ -10,4 +10,8 @@ def get_entries_length(
     entries_extension: bytes,
     entries_number: int,
 ) -> int:
+    entries_extension_bytearray = bytearray(entries_extension)
+    while len(entries_extension_bytearray) > 0:
+        size = __get_next_entry_size(entries_extension_bytearray)
+        __skip_entry(entries_extension_bytearray, size)
     pass
