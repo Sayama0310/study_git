7c3abaf01981816758b7691828fc54a435b40a59
Sayama0310 <montblanc3060@gmail.com>
2023-02-05 11:04:11 +0900

    [feat] Exttensionクラスを実装

--- git_index/extension.py
+++ git_index/extension.py
@@ -1,9 +1,16 @@
+import colorama
+from colorama import Fore
+
+#Initialize colorama
+colorama.init(autoreset=True)
+
+
 class Extension:
 
     def __init__(self, extension_bytes: bytes) -> None:
         self.__extension = extension_bytes
 
     def show(self):
-        print(Fore.BLUE + '============= ENTRIES ============')
-        print(Fore.BLUE + '==================================')
-        print()
+        print(Fore.YELLOW + '============ EXTENSION ===========')
+        print(self.__extension)
+        print(Fore.YELLOW + '==================================')
